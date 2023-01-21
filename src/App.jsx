f87a1936b7619aba03e4323eccbee5aacad33dbd
https://github.com/onecseven <onecseven@gmail.com>
2023-01-21 01:49:54 -0300

    testing picker component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react"
 import { Timer } from "./components/Timer"
-
-
-import { useTimerStore } from "./store/store"
 import { TaskList } from "./components/TasksLists/TaskList"
 import { TotalLength } from "./components/Timer/TotalLength"
+import { Picker } from "./components/Picker/Picker"
+
 export const App = () => {
   return (
     <>
       <div className="App">
-  
+        <Picker/>
         <TotalLength/>
         <Timer />
         <TaskList/>
