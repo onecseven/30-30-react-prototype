a99a7302eace299765145d34169506f5e2021f93
https://github.com/onecseven <onecseven@gmail.com>
2023-01-24 23:48:01 -0300

    typescript btch

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useCallback } from "react"
+import { Picker } from "./components/Picker/Picker"
+import { PickerButton } from "./components/PickerButton"
+import { TimerView } from "./components/TimerView"
+import { TopBar } from "./components/TopBar"
+
+export const App = () => {
+  const [showPicker, setPicker] = useState(false)
+  const swap = useCallback(() => {
+    setPicker(!showPicker)
+  }, [showPicker])
+
+  return (
+    <>
+      <div className="App">
+        <TopBar swap={swap} />
+        {showPicker ? <Picker swap={swap} /> : <TimerView />}
+      </div>
+    </>
+  )
+}
+
+export default App
