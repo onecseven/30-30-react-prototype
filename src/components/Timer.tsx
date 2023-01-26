032def69e4d817d775c8e0e80f94beb05c848036
https://github.com/onecseven <onecseven@gmail.com>
2023-01-26 16:51:46 -0300

    A

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -7,6 +7,7 @@ import { seconds_to_mmss } from "./TasksLists/seconds_to_mmss"
 import { TimerText } from "./Timer/TimerText"
 import { Triangle } from "./Timer/Triangle"
 import { ClassicTimerButtons } from "./ClassicTimerButtons"
+import { Playback } from "./Timer/Playback"
 
 // a is to b as x is to {y} (solves for y)
 const solve_for_y = (a: number, b: number, x: number) => {
@@ -18,12 +19,7 @@ export const MAX = 1600
 export const Timer = () => {
   let { length, remaining_seconds } = useTaskStor((state) => state)
   let fLen = MAX - solve_for_y(length, remaining_seconds, MAX)
-  let { dispatch, status } = useTimerStore((state) => state)
-  let play = () => dispatch("start", null)
-  let stop = () => dispatch("stop", null)
-  let send_to_bottom = () => dispatch("sendBottom", null)
   let cLen = seconds_to_mmss(remaining_seconds)
-  let isPlaying = status === "TIMER_ACTIVE"
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -33,8 +29,7 @@ export const Timer = () => {
       transform="scale(0.8)">
       <BGCircle />
       <ActiveCircle fLen={fLen} max={MAX} />
-      {isPlaying ? <Pause cb={stop} /> : <Triangle cb={play} />}
-
+      <Playback/>
       <TimerText rem={cLen} />
       <ClassicTimerButtons />
     </svg>
