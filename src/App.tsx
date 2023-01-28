4684e79858e0b35feb28a44ac2c7e4926a6e0d5d
https://github.com/onecseven <onecseven@gmail.com>
2023-01-27 23:47:04 -0300

    the title now changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { Picker } from "./components/Picker/Picker"
 import { TimerView } from "./components/TimerView"
 import { TopBar } from "./components/TopBar"
 import { useColor } from "./store/useColor"
+import { useTitle } from "./store/useTitle"
 
 export const App = () => {
   useColor()
+  useTitle()
   const [showPicker, setPicker] = useState(false)
   const swap = useCallback(() => {
     setPicker(!showPicker)
