86bc282656b500d13c2b5757a741e034a3fba643
https://github.com/onecseven <onecseven@gmail.com>
2023-01-28 00:02:44 -0300

    settings button added

--- TODO.md
+++ TODO.md
@@ -1,9 +1,7 @@
-
-
 ## TODO make our preferred timer setup
 
 ## TODO play sounds (chirp on task, zleda/ff on finish)
 
-## TODO settings tab
+## TODO add settingsView
 ### TODO mute sounds
 
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,19 +1,22 @@
 import React from "react"
 import { PickerButton } from "./Picker/PickerButton"
+import { GearIcon } from "./Shared/Icons"
 import { TaskListName } from "./TasksLists/TaskListName"
-
 
 interface TopBarProps {
   swap: () => void
 }
 
+const SettingsButton = () => (
+  <div className="medium-background settingsBtn">
+    <GearIcon x="0" y="0" />
+  </div>
+)
 
-
-export const TopBar = ({swap}: TopBarProps) => {
-  return (
-    <div className="topBar">
-      <PickerButton swap={swap} />
-      <TaskListName />
-    </div>
-  )
-}
+export const TopBar = ({ swap }: TopBarProps) => (
+  <div className="topBar">
+    <PickerButton swap={swap} />
+    <TaskListName />
+    <SettingsButton />
+  </div>
+)
