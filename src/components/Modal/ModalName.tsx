8eaa73ce351196609149d697713564bc4bb97d6b
https://github.com/onecseven <onecseven@gmail.com>
2023-02-08 18:48:07 -0300

    added functionality

--- src/components/Modal/ModalName.tsx
+++ src/components/Modal/ModalName.tsx
@@ -1,10 +1,16 @@
 import React from "react"
-
+import { useEditingTask } from "../../store/useEditingTask"
 
 export const ModalName = () => {
+  let [current, change] = useEditingTask()
+  let handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    change({ name: e.target.value.slice(0, 12) })
+  }
   return (
     <input
       type="text"
+      defaultValue={current.name}
+      onChange={handleChange}
       className="medium-background nameEditField"
     ></input>
   )
