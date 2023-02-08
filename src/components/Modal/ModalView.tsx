eaf433644b2664dc2c8d18b0be9bb76063905819
https://github.com/onecseven <onecseven@gmail.com>
2023-02-08 18:48:56 -0300

    new stuff

--- src/components/Modal/ModalView.tsx
+++ src/components/Modal/ModalView.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { useColor } from "../../store/useColor"
 import { Tabs } from "../Shared/Tabs/Tabs"
+import { BottomBar } from "./BottomBar"
 import { Modal } from "./Modal"
 import { ModalColor, ModalPicker } from "./ModalColor"
 import { ModalIcon } from "./ModalIcon"
@@ -10,12 +11,15 @@ import { ModalTime, ModalTimePicker } from "./ModalTime"
 
 export type ModalViews = "COLOR" | "ICON" | "NUMPAD"
 
-export const ModalView = () => {
+interface ModalViewProps {
+  swap: () => void
+}
+
+export const ModalView = ({swap}: ModalViewProps) => {
   const [view, setView] = useState<ModalViews>("NUMPAD")
   return (
     <Modal>
       <div className="topBar">
-        <ModalColor />
         <ModalName />
         <ModalTime />
       </div>
@@ -23,6 +27,7 @@ export const ModalView = () => {
       {view === "COLOR" && <ModalPicker />}
       {view === "ICON" && <ModalIcon/>}
       {view === "NUMPAD" && <ModalTimePicker/>}
+      <BottomBar swap={swap}/>
     </Modal>
   )
 }
