2927fb22beae5a99aa7e35a37134f8de4194abc6
https://github.com/onecseven <onecseven@gmail.com>
2023-02-08 18:48:30 -0300

    back button

--- src/components/Modal/BottomBar.tsx
+++ src/components/Modal/BottomBar.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { CheckMarkIcon } from "../Shared/icons/CheckMarkIcon";
 import { TrashIcon } from "../Shared/icons/TrashIcon";
 
-
-export const BottomBar = () => {
+interface BottomBarProps {
+  swap: () => void
+}
+//TODO TrashIcon functionality? A different CheckMark icon? idk
+export const BottomBar = ({swap}: BottomBarProps) => {
   return <div className="bottomBar">
     <li className="tab-li " style={{ marginRight: "2%" }}>
       <label
@@ -25,6 +28,7 @@ export const BottomBar = () => {
         aria-selected="true"
         aria-controls={"icon-panel"}
         tabIndex={0}
+        onClick={swap}
       >
         <CheckMarkIcon x={"0"} y={"0"} height="35" />
       </label>
