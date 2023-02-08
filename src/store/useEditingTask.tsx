6a5425e6369a6361adb68abe56bcd509b343d578
https://github.com/onecseven <onecseven@gmail.com>
2023-02-08 18:46:40 -0300

    hook for the modal editing things

---
+++ src/store/useEditingTask.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react"
+import { useTimerStore, useSettingsStore } from "./store"
+import { TaskChange } from "./taskListSlice"
+import { TaskStore } from "./taskSlice"
+
+export const useEditingTask = (): [TaskStore, (change: TaskChange["changes"]) => void] => {
+  let [tasks, dispatch] = useTimerStore((state) => [
+    state.tasks,
+    state.dispatch,
+  ])
+  let taskID = useSettingsStore((state) => state.editingTaskID)
+  let currentTask: TaskStore | null = null
+  if (taskID) {
+    currentTask = tasks.find((item) => item.id === taskID)
+    let sendPartialChange = (change: TaskChange["changes"]) =>
+      dispatch("editTask", {
+        id: taskID,
+        changes: change,
+      })
+    return [currentTask, sendPartialChange]
+  } else return [null, (change: TaskChange["changes"]) => console.error("No task selected. This shouldn't happen.")]
+}
